4d98b4cc14c92d96f0fb56cd02ffb77bc791c048
yu.kato <yukato.gakushuyo@gmail.com>
2018-12-18 12:28:53 +0000

    CRUD messages

--- db/migrate/20181216040402_create_tasks.rb
+++ db/migrate/20181216040402_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :conten
+      t.string :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20181216040402) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "conten"
+    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
